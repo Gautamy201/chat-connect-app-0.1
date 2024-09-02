b5cf6f7a03d7433e470e6e85ae5f23e940c6e66d
Gautam201 <g.143.p.y.com>
2024-09-02 23:00:29 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 function App() {
+  useEffect(() => {
+    // const socketConnection = io("http://localhost:8080/");
+    const socketConnection = io("https://chat-connect-app.netlify.app");
+
+    console.log(socketConnection);
+  }, []);
   return <>react App</>;
 }
 
