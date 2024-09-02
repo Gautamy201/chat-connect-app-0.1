03d63eec13f85943ad1f10a8888548957f8dd47b
Gautam201 <g.143.p.y.com>
2024-09-02 23:46:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect } from "react";
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 function App() {
   useEffect(() => {
+    const url = [
+      "https://chat-connect-app.netlify.app",
+      "http://localhost:8080",
+    ];
     // const socketConnection = io("http://localhost:8080/");
-    const socketConnection = io("https://chat-connect-app.netlify.app");
+    const socketConnection = io(url[0]);
 
     console.log(socketConnection);
   }, []);
